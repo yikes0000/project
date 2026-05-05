25a4173cd67e0f72c97b907ba519a56dae3e415c
yikes0000 <99128156+yikes0000@users.noreply.github.com>
2026-05-05 22:46:56 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+// 回到頂端按鈕功能
+const backToTop = document.getElementById("backToTop");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 200) {
+    backToTop.style.display = "block";
+  } else {
+    backToTop.style.display = "none";
+  }
+});
+
+backToTop.addEventListener("click", () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
